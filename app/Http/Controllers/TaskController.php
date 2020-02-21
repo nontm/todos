a22f8cc5b9f7fdb5bed8dd9ff7c3dae702c830fc
nontm <h.nozomiii@gmail.com>
2020-02-21 16:55:11 +0900

    chapter 10

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,24 +11,27 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    public function index(int $id)
+    public function index(Folder $folder)
     {
+
+
+        if (Auth::user()->id !== $folder->user_id) {
+            abort(403);
+        }
+    
         // すべてのフォルダを取得する
         //$folders = Folder::all();
         // ★ ユーザーのフォルダを取得する
         $folders = Auth::user()->folders()->get();
 
-        // 選ばれたフォルダを取得する
-        $current_folder = Folder::find($id);
-
         // 選ばれたフォルダに紐づくタスクを取得する
         //$tasks = Task::where('folder_id', $current_folder->id)->get();
         //モデルクラス書き換えにより↓でよくなる
-        $tasks = $current_folder->tasks()->get();
+        $tasks = $folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $folder->id,
             'tasks' => $tasks,
         ]);
     }
@@ -36,14 +39,14 @@ class TaskController extends Controller
     /**
      * GET /folders/{id}/tasks/create
      */
-    public function showCreateForm(int $id)
+    public function showCreateForm(Folder $folder)
     {
         return view('tasks/create', [
-            'folder_id' => $id
+            'folder_id' => $folder->id,
         ]);
     }
 
-    public function create(int $id, CreateTask $request)
+    public function create(Folder $folder, CreateTask $request)
     {
         $current_folder = Folder::find($id);
 
@@ -51,29 +54,27 @@ class TaskController extends Controller
         $task->title = $request->title;
         $task->due_date = $request->due_date;
 
-        $current_folder->tasks()->save($task);
+        $folder->tasks()->save($task);
 
         return redirect()->route('tasks.index', [
-            'id' => $current_folder->id,
+            'id' => $folder->id,
         ]);
     }
 
     /**
      * GET /folders/{id}/tasks/{task_id}/edit
      */
-    public function showEditForm(int $id, int $task_id)
+    public function showEditForm(Folder $folder, Task $task)
     {
-        $task = Task::find($task_id);
-
         return view('tasks/edit', [
             'task' => $task,
         ]);
     }
 
-    public function edit(int $id, int $task_id, EditTask $request)
+    public function edit(Folder $folder, Task $task, EditTask $request)
     {
         // 1
-        $task = Task::find($task_id);
+       // $task = Task::find($task_id);
     
         // 2
         $task->title = $request->title;
